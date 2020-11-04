f18389ce49d06ea9941a614b05a7311a8de0d45b
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-04 16:58:15 +0530

    Add project files.

---
+++ MaximumNumberProject/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/******************************************************************************  
+ *  Purpose: Find maximum from given integer double numbers and string using .Net Generics.
+ *
+ *
+ *
+ *  @author  Chetan
+ *  @version 1.0
+ *  @since   04-10-2020
+ *
+ ******************************************************************************/
+using System;
+
+namespace MaximumNumberProject
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Welcome to Maximum Number Project.");           
+        }
+    }
+}
