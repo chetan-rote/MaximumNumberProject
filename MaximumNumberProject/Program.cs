ff4ce31db135c0c0fc84fdf73df53887adcddb65
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-04 17:17:18 +0530

    [Chetan]Add. Find maximum from float numbers.

--- MaximumNumberProject/Program.cs
+++ MaximumNumberProject/Program.cs
@@ -24,14 +24,23 @@ namespace MaximumNumberProject
             ///Creates reference object.
             MaximumNumberCheck maximumNumber = new MaximumNumberCheck();
             Console.WriteLine("Welcome to Maximum Number Project.");
-            ///Reads 3 integer values from usere
+            ///Reads 3 integer values from user.
             Console.WriteLine("Enter three integers to find max of them.");
             int firstValue = Convert.ToInt32(Console.ReadLine());
             int secondValue = Convert.ToInt32(Console.ReadLine());
             int thirdValue = Convert.ToInt32(Console.ReadLine());
-            ///Passes Values as parameter to method.
-            int result = maximumNumber.MaximumIntegerNumber(firstValue, secondValue, thirdValue);
-            Console.WriteLine("Maximum integer value is:" + result);
+            ///Passes Values as parameter to method and stores max value in variable..
+            int maxIntNumber = maximumNumber.MaximumIntegerNumber(firstValue, secondValue, thirdValue);
+            Console.WriteLine("Maximum integer value is:" + maxIntNumber);
+
+            ///Reads 3 double values from user.
+            Console.WriteLine("Enter three float numbers.");
+            double firstDoubleValue = Convert.ToDouble(Console.ReadLine());
+            double secondDoubleValue = Convert.ToDouble(Console.ReadLine());
+            double thirdDoubleValue = Convert.ToDouble(Console.ReadLine());
+            ///Passes Values as parameter to method and stores max value in variable.
+            double maxDoubleNumber = maximumNumber.MaximumDoubleNumber(firstDoubleValue, secondDoubleValue, thirdDoubleValue);
+            Console.WriteLine("Maximum double value is: " + maxDoubleNumber);
         }
     }
 }
